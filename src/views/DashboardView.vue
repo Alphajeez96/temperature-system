651d6fee129b895ef35197a38fcd2c46aa7c1789
alphajeez <chimeremezechukwudire@gmail.com>
2023-12-07 13:12:07 +0100

    leaflet map setup

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,1 +1,55 @@
-<template>DASHBOARD</template>
+<template>
+  <div id="map"></div>
+</template>
+
+<script setup lang="ts">
+import L from 'leaflet'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  var map = L.map('map').setView([51.505, -0.09], 13)
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 20,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  })
+
+  var Stadia_AlidadeSmoothDark = L.tileLayer(
+    'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}',
+    {
+      maxZoom: 20,
+      attribution:
+        '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      ext: 'png'
+    }
+  )
+
+  var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution:
+      'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+  })
+
+  var Stadia_StamenToner = L.tileLayer(
+    'https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.{ext}',
+    {
+      maxZoom: 20,
+      attribution:
+        '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      ext: 'png'
+    }
+  ).addTo(map)
+
+  // You can perform any other actions here
+
+  L.marker([51.5, -0.09]).addTo(map)
+})
+</script>
+
+<style>
+@import 'leaflet/dist/leaflet.css';
+#map {
+  @apply bg-primary-black;
+  height: 380px;
+}
+</style>
